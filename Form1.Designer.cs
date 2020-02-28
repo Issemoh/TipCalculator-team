1e40385129adf814a2ff0d01f953e1af38a43e3f
Ice <your name>
2020-02-28 07:31:40 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace TipCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBill = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGuests = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTipPercent = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTip = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.chkRound = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(128, 263);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(107, 24);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(143, 37);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Restaurant Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bill amount";
+            // 
+            // txtBill
+            // 
+            this.txtBill.Location = new System.Drawing.Point(143, 76);
+            this.txtBill.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBill.Name = "txtBill";
+            this.txtBill.Size = new System.Drawing.Size(140, 20);
+            this.txtBill.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 113);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Number of People";
+            // 
+            // txtGuests
+            // 
+            this.txtGuests.Location = new System.Drawing.Point(143, 110);
+            this.txtGuests.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtGuests.Name = "txtGuests";
+            this.txtGuests.Size = new System.Drawing.Size(140, 20);
+            this.txtGuests.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 154);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tip Percent";
+            // 
+            // txtTipPercent
+            // 
+            this.txtTipPercent.Location = new System.Drawing.Point(143, 152);
+            this.txtTipPercent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTipPercent.Name = "txtTipPercent";
+            this.txtTipPercent.Size = new System.Drawing.Size(140, 20);
+            this.txtTipPercent.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(331, 41);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total each, before tip";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(475, 39);
+            this.txtSubtotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.ReadOnly = true;
+            this.txtSubtotal.Size = new System.Drawing.Size(129, 20);
+            this.txtSubtotal.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(331, 81);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tip";
+            // 
+            // txtTip
+            // 
+            this.txtTip.Location = new System.Drawing.Point(475, 79);
+            this.txtTip.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTip.Name = "txtTip";
+            this.txtTip.ReadOnly = true;
+            this.txtTip.Size = new System.Drawing.Size(129, 20);
+            this.txtTip.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(331, 119);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Total each, after tip";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(475, 116);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(129, 20);
+            this.txtTotal.TabIndex = 13;
+            // 
+            // chkRound
+            // 
+            this.chkRound.AutoSize = true;
+            this.chkRound.Location = new System.Drawing.Point(34, 215);
+            this.chkRound.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkRound.Name = "chkRound";
+            this.chkRound.Size = new System.Drawing.Size(171, 17);
+            this.chkRound.TabIndex = 15;
+            this.chkRound.Text = "Round tip up to nearest dollar?";
+            this.chkRound.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(331, 154);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Meal total Together";
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(475, 152);
+            this.txtFinal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.ReadOnly = true;
+            this.txtFinal.Size = new System.Drawing.Size(129, 20);
+            this.txtFinal.TabIndex = 16;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(304, 263);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(108, 24);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 323);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.chkRound);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTip);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTipPercent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGuests);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBill);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnCalculate);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Tip Calculator";
+      
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBill;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGuests;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTipPercent;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTip;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.CheckBox chkRound;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**********************************\
+*            2/20/2020             *
+*                                  *
+*        Andrew Terwilliger        *
+*         Group 4 Lab with:        *
+*          Jordan Hughes           *
+*          Isse, Mohamud           *
+*           Omar Mohamud           *
+*        Minneapolis College       *
+*    ITEC 2505-60 C# Programming   *
+*                                  *
+\**********************************/
+
+namespace TipCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Calculate button Method.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Non-Null assignment of tip variable.
+            double tip;
+
+            /*
+             * Here we're sending out each provided string to their respective validation method to
+             * return a valid form, such as the total bill as a double - and the number of Guests as an int.
+             */
+            if (!ValidateString(txtName.Text, out string name, out string textError))
+            {
+                MessageBox.Show(textError, "Restaraunt Name Error");
+                txtName.Focus();
+                return;
+            }
+            if (!ValidateDouble(txtBill.Text, out double SubTotal, out string subError))
+            {
+                MessageBox.Show(subError, "Bill Total Error");
+                txtBill.Focus();
+                return;
+            }
+            if (!ValidateInt(txtGuests.Text, out int Guests, out string intError))
+            {
+                MessageBox.Show(subError, "Number of Guests Error");
+                txtGuests.Focus();
+                return;
+            }
+            if (!ValidatePercent(txtTipPercent.Text, out double tipPercent, out string tipError))
+            {
+                MessageBox.Show(subError, "Tip Percent Error");
+                txtTipPercent.Focus();
+                return;
+            }
+
+            // Assignment of the total with no tip for each Guest.
+            double totalNoTip = SubTotal / Guests;
+            txtSubtotal.Text = totalNoTip.ToString("c");
+
+            // Quick assignment of tip before rounding.
+            double subTip = SubTotal * tipPercent;
+
+            // Checking if the tip should be rounded up.
+            if (chkRound.Checked)
+            {
+                tip = Math.Ceiling(subTip);
+            } else { tip = subTip; }
+            
+            // Assignment of the Tip textbox.
+            txtTip.Text = tip.ToString("c");
+
+            // Assignment of the Total with tip for each Guest.
+            double total = (SubTotal + tip) / Guests;
+            txtTotal.Text = total.ToString("c");
+
+            // Assignment of Meal Total.
+            txtFinal.Text = (SubTotal + tip).ToString("c");
+        }
+
+        // Input Validation, ValidateX refering to what we're validating for. i.e. ValidateString
+        private bool ValidateString(string text, out string name, out string textErrorMsg)
+        {
+            textErrorMsg = null;
+            name = text;
+            if (String.IsNullOrEmpty(text))
+            {
+                textErrorMsg = "Please enter valid Restaraunt Name.";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateDouble(string subTotal, out double SubTotal, out string subErrorMsg)
+        {
+            subErrorMsg = null;
+            SubTotal = 0;
+            try
+            {
+                SubTotal = Double.Parse(subTotal);
+                if (SubTotal >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    subErrorMsg = "Enter a positive Number";
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                subErrorMsg = "Enter numbers only.";
+                return false;
+            }
+            catch (OverflowException)
+            {
+                subErrorMsg = "Enter a smaller number.";
+                return false;
+            }
+        }
+        private bool ValidateInt(string guests, out int Guests, out string intErrorMsg)
+        {
+            intErrorMsg = null;
+            Guests = 0;
+            try
+            {
+                Guests = int.Parse(guests);
+                if (Guests >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    intErrorMsg = "Enter a positive Number";
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                intErrorMsg = "Enter numbers only.";
+                return false;
+            }
+            catch (OverflowException)
+            {
+                intErrorMsg = "Enter a smaller number.";
+                return false;
+            }
+        }
+        private bool ValidatePercent(string tip, out double tipPercent, out string tipErrorMsg)
+        {
+            tipErrorMsg = null;
+            tipPercent = 0;
+            try
+            {
+                tipPercent = Double.Parse(tip);
+                if (tipPercent >= 1 && tipPercent <= 100)
+                {
+                    tipPercent = tipPercent / 100;
+                    return true;
+                } else if (tipPercent < 1 && tipPercent >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    tipErrorMsg = "Enter a positive Number";
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                tipErrorMsg = "Enter numbers only.";
+                return false;
+            }
+            catch (OverflowException)
+            {
+                tipErrorMsg = "Enter a smaller number.";
+                return false;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
